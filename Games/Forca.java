79540100606085b1cb1f5cb7e67f12b63fffe058
JuliopProgrammer1 <julioreis1000.pro@gmail.com>
2020-07-04 14:12:25 -0300

    finishing the game Forca.

--- Games/Forca.java
+++ Games/Forca.java
@@ -4,12 +4,98 @@ import javax.swing.JOptionPane;
 public class Forca {
 
 	static Scanner input = new Scanner(System.in);
+	static char letraAtual;
+	static int errosTotais = 0;
 
 	/*Imprime abertura*/
 	static void abertura() {
 		System.out.println("*******************");
 		System.out.println("*  Jogo Da Forca  *");
 		System.out.println("*******************");
+		System.out.println("  _ _ _ _\n");
+		System.out.println(" |       |\n");
+		System.out.println(" |		\n");
+		System.out.println(" |		\n");
+		System.out.println(" |		\n");
+		System.out.println("_|_		\n");
+	}
+
+	static void imagemVitoria () {
+		System.out.println("\nBOA!\n");
+		System.out.println("  _ _ _ _\n");
+		System.out.println(" |       |            Obrigado!\n");
+		System.out.println(" |               (_) /\n");
+		System.out.println(" |              --|--\n");
+		System.out.println(" |               |-|\n");
+		System.out.println("_|_\n\n");
+	}
+
+	static void desenhaForca () {
+		if ( errosTotais == 0 ) {
+			System.out.println("  _ _ _ _\n");
+			System.out.println(" |       |\n");
+			System.out.println(" |		\n");
+			System.out.println(" |		\n");
+			System.out.println(" |		\n");
+			System.out.println("_|_		\n");
+		}
+		else if ( errosTotais == 1 ) {
+			System.out.println("  _ _ _ _\n");
+			System.out.println(" |       |\n");
+			System.out.println(" |      (_)\n");
+			System.out.println(" |		\n");
+			System.out.println(" |		\n");
+			System.out.println("_|_		\n");
+		}
+		else if ( errosTotais == 2 ) {
+			System.out.println("  _ _ _ _\n");
+			System.out.println(" |       |\n");
+			System.out.println(" |      (_)\n");
+			System.out.println(" |       |\n");
+			System.out.println(" |		\n");
+			System.out.println("_|_		\n");
+		}
+		else if ( errosTotais == 3 ) {
+			System.out.println("  _ _ _ _\n");
+			System.out.println(" |       |\n");
+			System.out.println(" |      (_)\n");
+			System.out.println(" |     --|\n");
+			System.out.println(" |		\n");
+			System.out.println("_|_		\n");
+		}
+		else if ( errosTotais == 4 ) {
+			System.out.println("  _ _ _ _\n");
+			System.out.println(" |       |\n");
+			System.out.println(" |      (_)\n");
+			System.out.println(" |     --|--\n");
+			System.out.println(" |		\n");
+			System.out.println("_|_		\n");
+		}
+		else if ( errosTotais == 5 ) {
+			System.out.println("\n  _ _ _ _   MORRI!\n");
+			System.out.println(" |       | /\n");
+			System.out.println(" |      (_)\n");
+			System.out.println(" |     --|--\n");
+			System.out.println(" |      |-|\n");
+			System.out.println("_|_		\n");
+			System.out.println("     GAMEOVER\n");
+		}
+	}
+
+	static void contaErros( String palavra, char[] chutes, int chutesdados ) {
+
+		boolean existe = false;
+
+		for ( int i = 0; i < palavra.length(); i++ ) {
+
+			if ( letraAtual == palavra.charAt(i) ) {
+				existe = true;
+				break;
+			}
+		}
+		if ( !existe ) {
+			errosTotais++;
+		}
 	}
 
 	/*Verifica se foi enforcado*/
@@ -45,11 +131,12 @@ public class Forca {
 			for ( int j = 0; j < palavra.length(); j++ ) {
 				if ( chutes[i] == palavra.charAt(j) ) {
 					existe = true;
+					acertos++;
 				}
 			}
-			if ( existe ) {
+			/*if ( existe ) {
 				acertos++;
-			}
+			}*/
 		}
 		return acertos >= palavra.length();
 	}
@@ -83,13 +170,24 @@ public class Forca {
 					System.out.print("_ ");
 				}
 			}
+
 			System.out.println();
 
 			/*Recebendo chutes*/
 			System.out.print("Qual letra?\nDigite: ");
 			chute = input.next().charAt(0);
+			System.out.println();
 			chutes[chutesdados] = chute;
+			letraAtual = chute;
 			chutesdados++;
+
+			contaErros(palavra, chutes, chutesdados);
+			desenhaForca();
+			System.out.println("Erros: " + errosTotais);
+
+			if ( ganhou(palavra, chutes, chutesdados) ) {
+				imagemVitoria();
+			}
 
 		} while ( !ganhou(palavra, chutes, chutesdados) && !enforcou(palavra, chutes, chutesdados) );
 	}
